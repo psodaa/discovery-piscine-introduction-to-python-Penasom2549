24694ad3b95e9f868c9bacb2e5e757d8c483494a
Penasom2549 <68030089@kmitl.ac.th>
2025-06-17 03:47:54 +0000

    name2.py

---
+++ cell01/ex01/name.py
@@ -0,0 +1,4 @@
+first_name = "Nattanan" 
+last_name = "Somphungthong"
+whole_name = first_name + last_name
+print(whole_name)
--- cell01/ex1.1/name.py
+++
@@ -1,4 +0,0 @@
-first_name = "Nattanan" 
-last_name = "Somphungthong"
-print(first_name+ " " +last_name)
-print(f"{first name} {last name}") // format string
