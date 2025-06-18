f4548b765400eb849769e04b12a23e3f627ce11d
Penasom2549 <68030089@kmitl.ac.th>
2025-06-18 06:34:28 +0000

    play_with_arrays.py

---
+++ cell05/ex02/play_with_arrays.py
@@ -0,0 +1,10 @@
+original_array = [2, 8, 9, 48, 8, 22, -12, 2]
+
+temp_array = [x + 2 for x in original_array]
+
+
+new_array = [x for x in temp_array if x > 5]
+
+
+print(original_array)
+print(new_array)
